ad0524e5cb32ee1e77cfd0fbffc844cefb9fd757
rajayogan <rajayogan27@gmail.com>
2017-01-26 20:47:45 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  @ViewChild('myvideo') myVideo: any;
+  
   title = 'app works!';
+  
+  targetpeer: any;
+  peer: any;
+  n = <any>navigator;
+  
+  ngOnInit() {
+    let video = this.myVideo.nativeElement;
+    let peerx: any;
+    this.n.getUserMedia = (this.n.getUserMedia || this.n.webkitGetUserMedia || this.n.mozGetUserMedia || this.n.msGetUserMedia);
+    this.n.getUserMedia({video:true, audio:true}, function(stream) {
+    peerx = new SimplePeer ({
+      initiator: location.hash === '#init',
+      trickle: false,
+      stream:stream
+    })
+    
+    peerx.on('signal', function(data) {
+      console.log(JSON.stringify(data));
+      
+      this.targetpeer = data;
+    })
+    
+    peerx.on('data', function(data) {
+      console.log('Recieved message:' + data);
+    })
+    
+    peerx.on('stream', function(stream) {
+      video.src = URL.createObjectURL(stream);
+      video.play();
+    })
+    
+    }, function(err){
+    console.log('Failed to get stream', err);
+    });
+    
+    setTimeout(() => {
+      this.peer = peerx;
+      console.log(this.peer);
+    }, 5000);
+  }
+  
+  connect() {
+    this.peer.signal(JSON.parse(this.targetpeer));
+  }
+  
+  message() {
+    this.peer.send('Hello world');
+  }
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+///<reference path="typings.d.ts"/>
+
 import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
